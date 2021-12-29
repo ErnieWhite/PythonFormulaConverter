2766f6afea029ffb624454365f966c92d3071d72
ErnieWhite <erniewhite@embarqmail.com>
2021-12-29 14:53:41 -0600

    Changed over to an OOP style

--- formulaconverter.py
+++ formulaconverter.py
@@ -47,127 +47,107 @@ def create_image(filepath):
     return ImageTk.PhotoImage(image)
 
 
+class FormulaEntry(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        self.textvariable = tk.StringVar()
+        self.image = create_image('eraser.png')
+        self.label = ttk.Label(self, text="Formula", width=11)
+        self.label.grid(row=0, column=0, sticky=tk.W)
+
+        self.entry = ttk.Entry(self, textvariable=self.textvariable)
+        self.entry.grid(row=0, column=1, sticky=(tk.E, tk.W))
+
+        self.clear_button = ttk.Button(self, text="Clear", command=lambda: app.formula_entry_clear(), image=self.image)
+        self.clear_button.grid(row=0, column=2, sticky=tk.E)
+
+        self.columnconfigure(1, weight=1)
+        self.grid(row=0, column=0, sticky=(tk.W, tk.E))
+
+    def get(self):
+        self.textvariable.get()
+
+    def set(self, v):
+        self.textvariable.set(v)
+
+
+class FormulaDisplay(ttk.Frame):
+    def __init__(self, container, text: str, row=0, column=0):
+        super().__init__(container)
+        # create the image to be used for the copy buttons
+        self.image = create_image('img.png')
+        self.textvariable = tk.StringVar()
+        self.label = ttk.Label(self, text=text, width=11)
+        self.label.grid(row=0, column=0, sticky=tk.W)
+
+        self.entry = ttk.Entry(self, state=tk.DISABLED, textvariable=self.textvariable)
+        self.entry.grid(row=0, column=1, sticky=(tk.W, tk.E))
+
+        self.copy_button = ttk.Button(self, text="Copy", command=lambda: pyperclip.copy(self.textvariable.get()), image=self.image)
+        self.copy_button.grid(row=0, column=2, sticky=tk.E)
+        self.columnconfigure(1, weight=1)
+        self.grid(row=row, column=column, sticky=(tk.W, tk.E))
+
+    def get(self):
+        self.textvariable.get()
+
+    def set(self, v):
+        self.textvariable.set(v)
+
+
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        # create the Tkinter Variables to access teh data
-        self.formula_value = tk.StringVar()
-        self.multiplier_value = tk.StringVar()
-        self.discount_value = tk.StringVar()
-        self.markup_value = tk.StringVar()
-        self.gross_profit_value = tk.StringVar()
-
-        # set the windows title bar icon
         window_icon = tk.PhotoImage(file="fc.png")
         self.iconphoto(True, window_icon)
 
-        # create the Vars
-        self.formula_value = tk.StringVar()
-
-        # create the image to be used for the copy buttons
-        self.copy_image = create_image('img.png')
-
-        # create the image to be used for the erase button
-        self.erase_image = create_image('eraser.png')
-
         self.frame = ttk.Frame(self, padding=(10, 10, 10, 10))
 
-        self.formula_label = ttk.Label(self.frame, text="Formula")
-        self.formula_label.grid(row=0, column=0, sticky=tk.W)
-
-        self.formula_entry = ttk.Entry(self.frame, textvariable=self.formula_value)
-        self.formula_entry.grid(row=0, column=1, sticky=(tk.E, tk.W))
-
-        self.formula_clear_button = ttk.Button(self.frame, text="Clear", command=self.formula_entry_clear, image=self.erase_image)
-        self.formula_clear_button.grid(row=0, column=2, sticky=tk.E)
-
-        multiplier_label = ttk.Label(self.frame, text="Multiplier")
-        multiplier_label.grid(row=1, column=0, sticky=tk.W)
-
-        multiplier_entry = ttk.Entry(self.frame, state=tk.DISABLED, textvariable=self.multiplier_value)
-        multiplier_entry.grid(row=1, column=1, sticky=(tk.W, tk.E))
-
-        multiplier_copy_button = ttk.Button(self.frame, text="Copy", command=self.copy_multiplier_formula, image=self.copy_image)
-        multiplier_copy_button.grid(row=1, column=2, sticky=tk.E)
-
-        discount_label = ttk.Label(self.frame, text="Discount")
-        discount_label.grid(row=2, column=0, sticky=tk.W)
-
-        discount_entry = ttk.Entry(self.frame, state=tk.DISABLED, textvariable=self.discount_value)
-        discount_entry.grid(row=2, column=1, sticky=(tk.W, tk.E))
-
-        discount_copy_button = ttk.Button(self.frame, text="Copy", command=self.copy_discount_formula, image=self.copy_image)
-        discount_copy_button.grid(row=2, column=2, sticky=tk.E)
-
-        markup_label = ttk.Label(self.frame, text="Markup")
-        markup_label.grid(row=3, column=0, sticky=tk.W)
-
-        markup_entry = ttk.Entry(self.frame, state=tk.DISABLED, textvariable=self.markup_value)
-        markup_entry.grid(row=3, column=1, sticky=(tk.W, tk.E))
-
-        markup_copy_button = ttk.Button(self.frame, text="Copy", command=self.copy_markup_formula, image=self.copy_image)
-        markup_copy_button.grid(row=3, column=2, sticky=tk.E)
-
-        gross_profit_label = ttk.Label(self.frame, text="Gross Profit", padding=(0, 0, 5, 0))
-        gross_profit_label.grid(row=4, column=0, sticky=tk.W)
-
-        gross_profit_entry = ttk.Entry(self.frame, state=tk.DISABLED, textvariable=self.gross_profit_value)
-        gross_profit_entry.grid(row=4, column=1, sticky=(tk.W, tk.E))
-
-        gross_profit_copy_button = ttk.Button(self.frame, text="Copy", command=self.copy_gross_profit_formula, image=self.copy_image)
-        gross_profit_copy_button.grid(row=4, column=2, sticky=tk.E)
+        self.formula_entry = FormulaEntry(self.frame)
+        self.multiplier_display = FormulaDisplay(self.frame, text="Multiplier", row=1, column=0)
+        self.discount_display = FormulaDisplay(self.frame, text="Discount", row=2, column=0)
+        self.markup_display = FormulaDisplay(self.frame, text="Markup", row=3, column=0)
+        self.gross_profit_display = FormulaDisplay(self.frame, text="Gross Profit", row=4, column=0)
 
         self.frame.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.E, tk.W))
-        self.frame.columnconfigure(1, weight=1)
+        self.frame.columnconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
 
-        self.formula_entry.bind("<KeyRelease>", self.do_something)
+        self.formula_entry.entry.bind("<KeyRelease>", self.do_something)
 
     def do_something(self, event):
         try:
-            formula = self.formula_value.get().upper()
+            formula = self.formula_entry.entry.get().upper()
             multiplier = find_multiplier(formula)
 
             multiplier_formula = find_multiplier_formula(multiplier)
-            self.multiplier_value.set(multiplier_formula)
+            self.multiplier_display.set(multiplier_formula)
 
             markup_formula = find_markup_formula(multiplier)
-            self.markup_value.set(markup_formula)
+            self.markup_display.set(markup_formula)
 
             discount_formula = find_discount_formula(multiplier)
-            self.discount_value.set(discount_formula)
+            self.discount_display.set(discount_formula)
 
             gross_profit_formula = find_gross_profit_formula(multiplier)
-            self.gross_profit_value.set(gross_profit_formula)
+            self.gross_profit_display.set(gross_profit_formula)
         except ZeroDivisionError:
-            self.clear_formulas()
+            self.clear_displays()
         except TypeError:
-            self.clear_formulas()
+            self.clear_displays()
         except ValueError:
-            self.clear_formulas()
-
-    def copy_multiplier_formula(self):
-        pyperclip.copy(self.multiplier_value.get())
-
-    def copy_discount_formula(self):
-        pyperclip.copy(self.discount_value.get())
-
-    def copy_gross_profit_formula(self):
-        pyperclip.copy(self.gross_profit_value.get())
-
-    def copy_markup_formula(self):
-        pyperclip.copy(self.markup_value.get())
+            self.clear_displays()
 
     def formula_entry_clear(self):
-        self.formula_value.set("")
-        self.clear_formulas()
+        self.formula_entry.set("")
+        self.clear_displays()
 
-    def clear_formulas(self):
-        self.multiplier_value.set('')
-        self.discount_value.set('')
-        self.markup_value.set('')
-        self.gross_profit_value.set('')
+    def clear_displays(self):
+        self.multiplier_display.set('')
+        self.discount_display.set('')
+        self.markup_display.set('')
+        self.gross_profit_display.set('')
 
 
 if __name__ == "__main__":
